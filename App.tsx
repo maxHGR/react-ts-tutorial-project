61df7d74bd159d4f7518dd6cb6a67e7d4d188f10
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-07 23:07:05 +0200

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react';
 import './style.css';
 
-let name: string;
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-/* type Person = {
-  name: string;
-  age?: number;
-};
-
-let person: Person[]; */
 
 const App = () => {
   return (
