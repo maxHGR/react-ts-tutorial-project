eb6a8f55008379fa08f71b6fe695e3a52673b7ce
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-07 22:34:19 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
