0b3d439a002dcb2011fc9cb5277b17c8d62d8635
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-07 22:52:23 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import './style.css';
 
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person[];
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello {role} </h1>
     </div>
   );
-}
+};
 
 export default App;
