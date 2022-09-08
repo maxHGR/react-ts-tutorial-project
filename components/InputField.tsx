e334496a94623dd3e6b66e5d80aa7f1576b112ad
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-08 22:32:52 +0200

    2 files modified

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
       <input type="input" placeholder="Enter a Task" className="input_box" />
